f874f027abceffa21f01cf19adcd7aef56b410c8
Roma <rkurenok99@gmail.com>
2022-04-24 20:48:01 +0300

    added authentication token parameters

---
+++ MessengerService/AuthOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MessengerService
+{
+    // класс для хранения настроек аутентификационного токена
+    public class AuthOptions
+    {
+        public const string ISSUER = "MessengerService"; // издатель токена
+        public const string AUDIENCE = "MessengerClient"; // потребитель токена
+        const string KEY = "ecivreS!8regnesseM!1927_tneilC!8regnesseM!1927_!!4646_!!!92"; // ключ для шифрации
+        public const int LIFETIME = 1; // время жизни токена - 1 минута
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
