530c88fdf5d4e3eb668fdbcde0bdbdce8a9341b9
Roma <rkurenok99@gmail.com>
2022-04-24 20:17:12 +0300

    added data context

---
+++ MessengerService/Models/MessengerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessengerService.Models
+{
+    public class MessengerContext : DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<Message> Messages { get; set; }
+        DbSet<Chat> Chats { get; set; }
+
+        public MessengerContext(DbContextOptions<MessengerContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
